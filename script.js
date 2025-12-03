b1aeb9dc8020f743626ef92822c5a7354b7efcc9
filipebezerra95 <filipebza2019@gmail.com>
2025-12-03 00:23:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const numberResult = document.querySelector("h4")
+const boxArticle = document.querySelector("article")
+const button = document.querySelector("button")
+
+
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+   const result = Math.floor(Math.random() * (max - min + 1)) + min
+    numberResult.innerHTML = result
+}
+
+function hiddenArticle(){
+        boxArticle.style.left = "50%";
+        boxArticle.style.transform = "translatex(-50%)"
+}
+
+function byeArticle(){
+    boxArticle.style.left = "-930px"
+    boxArticle.style.transform = "translatex(0%)"
+}
+ 
+ button.addEventListener("click", hiddenArticle)
+ 
+
